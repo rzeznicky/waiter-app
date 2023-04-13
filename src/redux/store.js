6bfee6cbd903e7f7878fa225ba0ad983c85ee811
Pawel Rzeznicki <p.rzeznicky@gmail.com>
2023-04-13 17:39:18 +0200

    Add tablesRedux.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import initialState from './initialState';
+import tablesReducer from './tablesRedux';
 
 const subreducers = {
-
+  tables: tablesReducer,
 }
 
 const reducer = combineReducers(subreducers);
