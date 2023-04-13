28c0b08d0afa948e9ab9168d51d1d5a9a6c4554f
Pawel Rzeznicki <p.rzeznicky@gmail.com>
2023-04-13 21:50:39 +0200

    Add json server and fetch tables to state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import NotFound from './components/pages/NotFound/NotFound';
 import { Container } from 'react-bootstrap';
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchTables } from './redux/tablesRedux';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(fetchTables()), [dispatch]);
+
   return (
     <Container>
       <Header />
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -1,11 +1,25 @@
 //selectors
+export const getAllTables = state => state.tables;
+export const getTableById = ({ tables }, tableId) => tables.find(table => table.id === tableId);
 
 // actions
 const createActionName = actionName => `app/tables/${actionName}`;
+const UPDATE_TABLES = createActionName('UPDATE_BOOKS');
 
 // action creators
+export const updateTables = payload => ({ type: UPDATE_TABLES, payload });
+export const fetchTables = () => {
+  return (dispatch) => {
+    fetch('http://localhost:3131/api/tables')
+      .then(res => res.json())
+      .then(tables => dispatch(updateTables(tables)));
+  }
+};
+
 const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
+    case UPDATE_TABLES:
+      return [...action.payload];
     default:
       return statePart;
   };
